ac013d42660191ed7e64ebc84c9d51cdce1839b5
roddonalds <zaffpontoquarenta@gmail.com>
2024-03-20 22:47:20 +0000

    up

--- platforms/android/cordova/lib/AndroidManifest.js
+++ platforms/android/cordova/lib/AndroidManifest.js
@@ -37,6 +37,7 @@ class AndroidManifest {
     }
 
     setVersionName (versionName) {
+        this.doc.getroot().attrib['android:exported'] = true;
         this.doc.getroot().attrib['android:versionName'] = versionName;
         return this;
     }
