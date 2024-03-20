dd931b3ae3008c1bd87699deecf6342932f9b36f
roddonalds <zaffpontoquarenta@gmail.com>
2024-03-20 23:07:20 +0000

    up

--- scripts/uploadCdn.js
+++ scripts/uploadCdn.js
@@ -8,14 +8,28 @@ const pathDistDir = '/root/app/dist',
                 remoteAddr = 'ticket4.fun',
                     remoteCdnAddr = 'cdn.ticket4.fun';
 
+const remotePathFiles = '/home/deploy/ticket4.fun/_proj/cdn',
+        remotePathApk = `${remotePathFiles}/debugLatest.apk`,
+            remotePathAab = `${remotePathFiles}/releaseLatest.aab`;
+
 ;(() => {
 
-    console.log('Uploading debugLatest.apk to remote CDN server')
-    sjs.exec(`scp ${pathDistDir}/debugLatest.apk ${remoteUser}@${remoteAddr}:${remoteDistDir}`)
-    console.log(`https://${remoteCdnAddr}/debugLatest.apk`)
+    console.log('Removing current .apk file.')
+    sjs.exec(`ssh ${remoteUser}@${remoteAddr} 'rm ${remotePathApk}'`)
 
-    console.log('Uploading releaseLatest.aab to remote CDN server')
-    sjs.exec(`scp ${pathDistDir}/releaseLatest.aab ${remoteUser}@${remoteAddr}:${remoteDistDir}`)
-    console.log(`https://${remoteCdnAddr}/releaseLatest.aab`)
+    console.log('Removing current .aab file.')
+    sjs.exec(`ssh ${remoteUser}@${remoteAddr} 'rm ${remotePathAab}'`)
+
+    setTimeout(() => {
+
+        console.log('Uploading debugLatest.apk to remote CDN server')
+        sjs.exec(`scp ${pathDistDir}/debugLatest.apk ${remoteUser}@${remoteAddr}:${remoteDistDir}`)
+        console.log(`https://${remoteCdnAddr}/debugLatest.apk`)
+    
+        console.log('Uploading releaseLatest.aab to remote CDN server')
+        sjs.exec(`scp ${pathDistDir}/releaseLatest.aab ${remoteUser}@${remoteAddr}:${remoteDistDir}`)
+        console.log(`https://${remoteCdnAddr}/releaseLatest.aab`)
+
+    }, 1500)
 
 })();
